3aeb730b56a57094ea9f10476ed9d17837d63552
rodrigog84 <rodrigog.84@gmail.com>
2017-08-24 23:44:00 -0300

    agrega libro de compras

--- core/application/controllers/facturas.php
+++ core/application/controllers/facturas.php
@@ -355,7 +355,11 @@ public function show_dte($idfactura){
 		];
 
 
-		$LibroCompraVenta->agregarVentasCSV('./facturacion_electronica/tmp/' . $nombre_archivo_csv);
+		if($tipo_libro == 'VENTA'){
+			$LibroCompraVenta->agregarVentasCSV('./facturacion_electronica/tmp/' . $nombre_archivo_csv);
+		}else{
+			$LibroCompraVenta->agregarComprasCSV('./facturacion_electronica/tmp/' . $nombre_archivo_csv);
+		}
 
 		// enviar libro de compras y mostrar resultado del envío: track id o bien =false si hubo error
 		$LibroCompraVenta->setCaratula($caratula);
